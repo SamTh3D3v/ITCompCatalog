5199795b591cc7952a7719bdc7c85ba29e728424
samthedev <abdelkhalekdev@gmail.com>
2014-12-30 12:12:56 +0100

    Defines Properties and Bind THem FavView

--- ITCompCatalogue/ITCompCatalogue.Shared/ViewModel/FavoriteCoursesViewModel.cs
+++ ITCompCatalogue/ITCompCatalogue.Shared/ViewModel/FavoriteCoursesViewModel.cs
@@ -1,17 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 using GalaSoft.MvvmLight;
+using ITCompCatalogue.Model;
 
 namespace ITCompCatalogue.ViewModel
 {
     class FavoriteCoursesViewModel:ViewModelBase
     {
         #region Fields
-        
+        private ObservableCollection<Cour> _listFavoriteCourses;
         #endregion
         #region Properties
-        
+        public ObservableCollection<Cour> ListFavoriteCourses
+        {
+            get
+            {
+                return _listFavoriteCourses;
+            }
+
+            set
+            {
+                if (_listFavoriteCourses == value)
+                {
+                    return;
+                }
+
+                _listFavoriteCourses = value;
+                RaisePropertyChanged();
+            }
+        }
         #endregion
         #region Commands
         
