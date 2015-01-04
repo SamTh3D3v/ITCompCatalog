2f7935c9099d0a10b8235a507389c8639198e140
samthedev <abdelkhalekdev@gmail.com>
2015-01-04 10:38:05 +0100

    Add Views To Metro App Project

---
+++ ITCompCatalogue/ITCompCatalogue.Windows/Converters/ListViewItemClickedConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using ITCompCatalogue.Model;
+
+namespace ITCompCatalogue.Converters
+{
+    public class ListViewItemClickedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var args = value as ItemClickEventArgs;
+            if (args != null)
+            {
+                var cursusCour = args.ClickedItem as CursusCour;
+                if (cursusCour != null)
+                    return cursusCour.Cour;
+                else
+                    return args.ClickedItem as Cour;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
