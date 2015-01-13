82b54a437feb4d1b3ff3963bfe84f588af41d232
samthedev <o_elhamer@esi.Dz>
2015-01-13 09:55:56 +0100

    Reset Commit

---
+++ FakeItcompService/IItcompService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeItcompService
+{
+    public interface IItcompService
+    {
+        List<CoursSchedule> GetCoursScheduleByCursusId(long cursusId);
+        List<CoursSchedule> GetCoursScheduleByCoursId(long coursId);
+    }
+}
---
+++ FakeItcompService/ItcompService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeItcompService
+{
+    public class ItcompService:IItcompService
+    {
+        public List<CoursSchedule> GetCoursScheduleByCursusId(long cursusId)
+        {
+            return new List<CoursSchedule>()
+            {
+                new CoursSchedule(3, new DateTime(2013,11,5),new DateTime(2013,12,5)),
+                new CoursSchedule(4, new DateTime(2013,07,15),new DateTime(2013,07,25)),
+                new CoursSchedule(5, new DateTime(2012,10,5),new DateTime(2012,11,10)),
+            };
+        }
+
+        public List<CoursSchedule> GetCoursScheduleByCoursId(long coursId)
+        {
+            return new List<CoursSchedule>()
+            {
+                new CoursSchedule(4, new DateTime(2013,07,15),new DateTime(2013,07,25)),
+                new CoursSchedule(4, new DateTime(2012,10,5),new DateTime(2012,11,10)),
+            };
+        }
+    }
+
+    public class CoursSchedule
+    {
+        public long CoursId { get; set; }
+        public DateTime DateDebut { get; set; }
+        public DateTime DateFin { get; set; }
+
+        public CoursSchedule(long coursId,DateTime dateDebut,DateTime dateFin)
+        {
+            CoursId = coursId;
+            DateDebut = dateDebut;
+            DateFin = dateFin;
+        }
+    }
+}
